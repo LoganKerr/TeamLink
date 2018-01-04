88e7c61d8be3bd62cf7595ab55a28caf758228a3
Logan Kerr <lkerr2@pride.hofstra.edu>
2018-01-04 00:32:49 -0500

    createteam now refills on error

--- createteam.php
+++ createteam.php
@@ -72,8 +72,10 @@
             <div class="panel-body">
                 <div class="container">
                     <form method="post" action="/createteam.php">
-                        <p><label>Title:</label><input class="textbox" name="title" type="text" /></p>
-                        <p><label>Description:</label><textarea name="description"></textarea></p>
+                        <p><label>Title:</label><input class="textbox" name="title" type="text" value='<?php if (isset($title)) { echo htmlentities($title, ENT_QUOTES); } ?>'/>
+                        <?php echo(isset($error['title']))?$error['title']:""; ?></p>
+                        <p><label>Description:</label><textarea name="description"><?php if (isset($description)) { echo htmlentities($description, ENT_QUOTES); } ?></textarea>
+                        <?php echo(isset($error['description']))?$error['description']:""; ?></p>
                         <div class="submit-button"><input class="btn btn-primary btn-block" type="submit" value="Create Team" /></div>
                     </form>
                 </div>
