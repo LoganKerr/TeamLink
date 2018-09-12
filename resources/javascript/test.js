a77f9ee4fc3c14530345d192e460f3f1bb4e2165
Logan Kerr <lkerr1998@gmail.com>
2018-09-11 22:19:29 -0400

    Added more comments to test.js

--- resources/javascript/test.js
+++ resources/javascript/test.js
@@ -6,9 +6,11 @@ function addField(e)
     var tempId = boxId;
     // finds end of role list
     var end = document.getElementById("role_list");
+    // textbox for new role name
     var inputRoleName = document.createElement("input");
     inputRoleName.name="role_new"+tempId;
     inputRoleName.id="role_new"+tempId;
+    // button to remove new role
     var removeRoleButton = document.createElement("button");
     removeRoleButton.id = "role_new_button"+tempId;
     removeRoleButton.type = "button";
@@ -17,19 +19,22 @@ function addField(e)
     end.appendChild(inputRoleName);
     end.appendChild(removeRoleButton);
     end.appendChild(document.createElement("br"));
-    removeRoleButton.addEventListener("click", function() { console.log(tempId); document.getElementById("role_new"+tempId).remove(); document.getElementById("role_new_button"+tempId).remove();  });
+    removeRoleButton.addEventListener("click", function() { document.getElementById("role_new"+tempId).remove(); document.getElementById("role_new_button"+tempId).remove();  });
     boxId++;
 }
+// Disables/enables existing role. Disabled roles will be removed on submit
 function removeRole(e, id)
 {
     var role_name = document.getElementById("role_name"+id);
     var role_button = document.getElementById("role_button"+id);
+    // re-enables role
     if (role_button.value == "remove")
     {
         role_name.disabled=false;
         role_name.style="";
         role_button.value="";
     }
+    // sets role to disabled for removal
     else
     {
         var removeRole = document.createElement("input");
@@ -41,6 +46,7 @@ function removeRole(e, id)
         role_button.value="remove";
     }
 }
+// sets team to be deleted from database
 function setDeletedTeam(e, id)
 {
     document.getElementById('team').value=id;
