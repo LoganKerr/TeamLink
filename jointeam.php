87139d29567126a6557fe9ce2838d7be470672dc
Logan Kerr <lkerr1998@gmail.com>
2018-09-22 14:41:44 -0400

    Applying for a role now works on the front-end and sends to back end

--- jointeam.php
+++ jointeam.php
@@ -14,7 +14,7 @@
     
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-        //
+        var_dump($_POST);
     }
     
     $user_id = $_SESSION['user_id'];
@@ -23,7 +23,7 @@
     $search_wildcard = "%".$search."%";
     // get teams that are similar to searched value
     //$stmt = $conn->prepare("SELECT `firstName`, `lastName`, `title`, `description` FROM `teams` INNER JOIN `users` ON teams.`owner`=users.`id` WHERE `public` AND (`title` LIKE ? OR `description` LIKE ? OR CONCAT(`firstName`, ' ', `lastName`) LIKE ?)");
-    $stmt = $conn->prepare("SELECT `firstName`, `lastName`, `title`, `teams`.`description`, `roles`.`role` FROM `roles` INNER JOIN `teams` ON `roles`.`team_id`=`teams`.`id` INNER JOIN `users` ON `teams`.`owner`=`users`.`id` WHERE `public` AND (`title` LIKE ? OR `description` LIKE ? OR `role` LIKE ? OR CONCAT(`firstName`, ' ', `lastName`) LIKE ?)");
+    $stmt = $conn->prepare("SELECT `firstName`, `lastName`, `title`, `teams`.`description`, `roles`.`id`, `roles`.`role` FROM `roles` INNER JOIN `teams` ON `roles`.`team_id`=`teams`.`id` INNER JOIN `users` ON `teams`.`owner`=`users`.`id` WHERE `public` AND (`title` LIKE ? OR `description` LIKE ? OR `role` LIKE ? OR CONCAT(`firstName`, ' ', `lastName`) LIKE ?)");
     $stmt->bind_param("ssss", $search_wildcard, $search_wildcard, $search_wildcard, $search_wildcard);
     $stmt->execute();
     $res = $stmt->get_result();
