0324dc44e805ac88d1dc1914f21371510abbb9fe
Logan Kerr <lkerr1998@gmail.com>
2018-10-18 23:38:12 -0400

    Finished very procedural and rudimentary signup script

--- scripts/signup.php
+++ scripts/signup.php
@@ -4,6 +4,7 @@
 
     // not done yet
     $done = false;
+    $data = array();
 
     echo "Account signup script\n";
     $handle = fopen("php://stdin", "r");
@@ -12,16 +13,16 @@
     while (!$done) {
         echo "Email: ";
         $line = fgets($handle);
-        $email = trim($line);
+        $data["email"] = trim($line);
         // validate email is not empty
-        if (empty($email)) {
+        if (empty($data["email"])) {
             echo "ERROR: Email cannot be empty.";
             break;
         }
         // TODO: validate email with regex
         // validate email is not already in use
         $stmt = $conn->prepare("SELECT `email` FROM `users` WHERE `email`=?");
-        $stmt->bind_param("s", $email);
+        $stmt->bind_param("s", $data["email"]);
         $stmt->execute();
         $res = $stmt->get_result();
         if ($res->num_rows != 0) {
@@ -30,83 +31,98 @@
         }
         echo "University: ";
         $line = fgets($handle);
-        $university = trim($line);
+        $data["university"] = trim($line);
         // validate university in database
-        $stmt = $conn->prepare("SELECT `id` FROM `universities` WHERE `title`=?");
-        $stmt->bind_param("s", $university);
+        $stmt = $conn->prepare("SELECT `id` FROM `universities` WHERE LOWER(`title`)=LOWER(?)");
+        $stmt->bind_param("s", $data["university"]);
         $stmt->execute();
         $res = $stmt->get_result();
         if ($res->num_rows == 0) {
             echo "ERROR: University not found";
             break;
         }
+        $row = $res->fetch_assoc();
+        $data["university"] = $row["id"];
+        echo "First Name: ";
+        $line = fgets($handle);
+        $data["firstName"] = trim($line);
+        echo "Last Name: ";
+        $line = fgets($handle);
+        $data["lastName"] = trim($line);
+        echo "Password: ";
+        $line = fgets($handle);
+        $pass = trim($line);
+        $data["passhash"] = password_hash($pass, PASSWORD_DEFAULT);
         echo "Role (student/faculty): ";
         $line = fgets($handle);
-        $role = trim($line);
+        $data["role"] = trim($line);
         // forces lowercase
-        $role = strtolower($role);
-        if($role != "student" && $role != "faculty")
+        $data["role"] = strtolower($data["role"]);
+        if($data["role"] != "student" && $data["role"] != "faculty")
         {
             echo "ERROR: Invalid role";
             break;
         }
-        echo "First Name: ";
-        $line = fgets($handle);
-        $first = trim($line);
-        echo "Last Name: ";
-        $line = fgets($handle);
-        $last = trim($line);
-        echo "Password: ";
-        $line = fgets($handle);
-        $pass = trim($line);
-        $hash = password_hash($pass, PASSWORD_DEFAULT);
-        // TODO: ONLY ASK DEPARTMENT/MAJOR/INTERESTS DEPENDING ON ROLE
-        // TODO: MAYBE CHANGE TO LOOP THROUGH ARRAY OR DATABASE FOR WHAT TO ASK
-        echo "Department: ";
-        $line = fgets($handle);
-        $department = trim($line);
-        echo "Major: ";
-        $line = fgets($handle);
-        $major = trim($line);
-        echo "Interests: ";
-        $line = fgets($handle);
-        $interests = trim($line);
-        if ($role == "Student") {
+        // faculty specific fields
+        if ($data["role"] == "faculty") {
+            echo "Department: ";
+            $line = fgets($handle);
+            $data["department"] = trim($line);
+        }
+        // student specific fields
+        if ($data["role"] == "student") {
+            echo "Major: ";
+            $line = fgets($handle);
+            $data["major"] = trim($line);
+            echo "Interests: ";
+            $line = fgets($handle);
+            $data["interests"] = trim($line);
+        }
+        $done = true;
+    }
+    fclose($handle);
+    // insert data into database
+    if ($done == true)
+    {
+        if ($data["role"] == "student")
+        {
             $stmt = $conn->prepare("INSERT INTO `students` (`major`, `interests`) VALUES (?, ?)");
-            $stmt->bind_param("ss", $major, $interests);
+            $stmt->bind_param("ss", $data["major"], $data["interests"]);
             if (!$stmt->execute()) {
                 echo "Registration error: " . $stmt->error;
             } else {
                 $student_id = $stmt->insert_id;
-                $stmt = $conn->prepare("INSERT INTO `users` (`email`, `university_id`, `firstName`, `lastName`, `passHash`, `student_id`) VALUES (?, (SELECT `id` FROM `universities` WHERE `title`=?), ?, ?, ?, ?)");
-                $stmt->bind_param("sssssi", $email, $university, $firstName, $lastName, $hash, $student_id);
+                $stmt = $conn->prepare("INSERT INTO `users` (`email`, `university_id`, `firstName`, `lastName`, `passHash`, `student_id`) VALUES (?, ?, ?, ?, ?, ?)");
+                $stmt->bind_param("sssssi", $data["email"], $data["university"], $data["firstName"], $data["lastName"], $data["passhash"], $student_id);
 
                 if ($stmt->execute()) {
-                    header("Location: index.php");
-                    exit();
-                } else {
                     echo "Registration error: " . $stmt->error;
                 }
+                else
+                {
+                    echo "Account successfully created";
+                }
             }
-        } elseif ($role == "Faculty") {
+        } elseif ($data["role"] == "faculty")
+        {
             $stmt = $conn->prepare("INSERT INTO `faculty` (`department`) VALUES (?)");
-            $stmt->bind_param("s", $department);
+            $stmt->bind_param("s", $data["department"]);
             if (!$stmt->execute()) {
                 echo "Registration error: " . $stmt->error;
             } else {
                 $faculty_id = $conn->insert_id;
-                $stmt = $conn->prepare("INSERT INTO `users` (`email`, `university_id`, `firstName`, `lastName`, `passHash`, `faculty_id`) VALUES (?, (SELECT `id` FROM `universities` WHERE `title`=?), ?, ?, ?, ?)");
-                $stmt->bind_param("sssssi", $email, $firstName, $lastName, $hash, $faculty_id);
+                $stmt = $conn->prepare("INSERT INTO `users` (`email`, `university_id`, `firstName`, `lastName`, `passHash`, `faculty_id`) VALUES (?, ?, ?, ?, ?, ?)");
+                $stmt->bind_param("sssssi", $data["email"], $data["university"], $data["firstName"], $data["lastName"], $data["passhash"], $faculty_id);
 
-                if ($stmt->execute()) {
-                    header("Location: index.php");
-                    exit();
-                } else {
+                if (!$stmt->execute()) {
                     echo "Registration error: " . $stmt->error;
+                }
+                else
+                {
+                    echo "Account successfully created";
                 }
             }
         }
     }
-    fclose($handle);
     ?>
 
