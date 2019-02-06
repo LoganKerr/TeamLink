00077abf7f26df4703af559ecbfe239a61d6a1a0
Derrick Hamilton <derrickhamilton212@gmail.com>
2019-02-05 23:25:16 -0500

    added javascript to delete team using ajax

---
+++ resources/javascript/edit_team_ajax.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$("#delete-team").click(function() {
+		var team_id = document.getElementById("team-id");
+		var id = team_id.value;
+		$.post("req_handler.php",
+		{
+			action: "delete_team",
+			id: id
+		},
+		function(data, status) {
+			window.location.reload();
+		}
+	});
+});
