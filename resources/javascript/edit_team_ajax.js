5267cf1b129d7c824f6f0f15a62375c27a62898b
Derrick Hamilton <derrickhamilton212@gmail.com>
2019-02-06 22:51:26 -0500

    added jquery ajax call for changing description on a team

--- resources/javascript/edit_team_ajax.js
+++ resources/javascript/edit_team_ajax.js
@@ -39,4 +39,29 @@ $(document).ready(function() {
 		$(".title-textbox").hide();
 		$("#team-title").show();
 	});
+	$("#desc-content").click(function() {
+		$("#desc-textarea").show();
+		$("#desc-content").hide();
+		$("#desc-confirm").removeClass("hidden");
+		$("#desc-remove").removeClass("hidden");
+	});
+	$("#desc-confirm").click(function() {
+		var desc = $("#desc-textarea").val();
+		var id = $("#team-id").val();
+		$.post("req_handler.php",
+		{
+			action: "change_desc",
+			desc: desc,
+			id: id
+		},
+		function(data, status) {
+			window.location.reload();
+		});
+	});
+	$("#desc-remove").click(function() {
+		$("#desc-textarea").hide();
+		$("#desc-content").show();
+		$("#desc-confirm").addClass("hidden");
+		$("#desc-remove").addClass("hidden");
+	});
 });
