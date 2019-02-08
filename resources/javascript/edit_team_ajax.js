a00c708498a26e7b54ed612f01d2a8a01c717cd5
Derrick Hamilton <derrickhamilton212@gmail.com>
2019-02-07 22:03:13 -0500

    done playing with the textbox for now

--- resources/javascript/edit_team_ajax.js
+++ resources/javascript/edit_team_ajax.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	// Will send ajax request on click of the delete team button
 	$("#delete-team").click(function() {
 		var id = $("#team-id").val();
 		$.post("req_handler.php",
@@ -10,18 +11,22 @@ $(document).ready(function() {
 			window.location.reload();
 		});
 	});
-	$("#team-title").mouseenter(function() {
-		$("#title-symbol").removeClass("hidden");
+	// Toggle the style of the team title on hover
+	$("#team-title").hover(function() {
+		$("#title-symbol").css("color", "#717b8c");
 		$("#team-title").css("border", "3px solid #717b8c");
-	});
-	$("#team-title").mouseleave(function() {
-		$("#title-symbol").addClass("hidden");
+	},
+	function() {
+		$("#title-symbol").css("color", "rgba(113, 123, 140, 0.4)");
 		$("#team-title").css("border", "3px solid white");
 	});
-	$("#team-title").click(function() {
+	// Will replace the team title with a textbox to change the title
+	$("#title-symbol").click(function() {
 		$(".title-textbox").removeClass("hidden");
+		$("#title-content").focus();
 		$("#team-title").addClass("hidden");
 	});
+	// Performs ajax call to update the title of the team
 	$("#title-confirm").click(function() {
 		var title = $("#title-content").val();
         var id = $("#team-id").val();
@@ -35,16 +40,19 @@ $(document).ready(function() {
 			window.location.reload();
 		});
 	});
+	// Will replace the title changing textbox with the original content
 	$("#title-remove").click(function() {
 		$(".title-textbox").addClass("hidden");
 		$("#team-title").removeClass("hidden");
 	});
+	// Will replace the description text with a textarea to edit the description
 	$("#desc-content").click(function() {
 		$("#desc-textarea").removeClass("hidden");
 		$("#desc-content").addClass("hidden");
 		$("#desc-confirm").removeClass("hidden");
 		$("#desc-remove").removeClass("hidden");
 	});
+	// Performs the ajax request to update the description of the team
 	$("#desc-confirm").click(function() {
 		var desc = $("#desc-textarea").val();
 		var id = $("#team-id").val();
